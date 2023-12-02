5bd0e4bce280866ce0fd96de47a5a7b3b87e4e28
vasil tatarov <vasiltatarov3@gmail.com>
2023-12-02 21:38:43 +0200

    add category controller, icons and new css

---
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿namespace BulkyWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        this.db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var categories = db.Categories.ToList();
+
+        return this.View(categories);
+    }
+}
--- Bulky/BulkyWeb/GlobalUsings.cs
+++ Bulky/BulkyWeb/GlobalUsings.cs
@@ -1,5 +1,8 @@
-﻿global using System.Diagnostics;
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.Diagnostics;
 
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
 
+global using BulkyWeb.Data;
 global using BulkyWeb.Models;
