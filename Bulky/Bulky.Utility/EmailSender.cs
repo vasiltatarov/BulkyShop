1ddc72bb7753cc557f82fa62d172ee07ef9902f8
vasil tatarov <vasiltatarov3@gmail.com>
2023-12-31 11:26:56 +0200

    implement SendGrid

--- Bulky/Bulky.Utility/EmailSender.cs
+++ Bulky/Bulky.Utility/EmailSender.cs
@@ -1,12 +1,29 @@
 ﻿namespace Bulky.Utility;
 
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
 
 public class EmailSender : IEmailSender
 {
+    public EmailSender(IConfiguration configuration)
+    {
+        this.SendGridSecretKey = configuration.GetValue<string>("SendGrid:SecretKey");
+    }
+
+    public string SendGridSecretKey { get; set; }
+
     public Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
         return Task.CompletedTask;
+
+        var client = new SendGridClient(this.SendGridSecretKey);
+        var from = new EmailAddress("TestUser@gmail.com", "My Website");
+        var to = new EmailAddress(email);
+        var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+        return client.SendEmailAsync(message);
     }
 }
